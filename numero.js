8396082b13c6dea410a76fe505b1c25fd3dcaeb6
LiJnD <luico199@gmal.com>
2022-10-11 15:15:33 -0500

    punto2

--- numero.js
+++ numero.js
@@ -12,7 +12,7 @@ switch (actividad) {
         let contador1 = 1;
         function calcula1() {
             while (autos > "0") {
-                const placas = prompt("Ingresa el ultimo digito de la placa. #"+contador1);
+                const placas = prompt("Ingresa el ultimo digito de la placa. #" + contador1);
                 alert("siguiente")
                 if (placas == 1 || placas == 2) {
                     amarillo++;
@@ -38,10 +38,57 @@ switch (actividad) {
         }
         calcula1();
         alert("La cantidad de autos amarillos son: " + amarillo);
-        alert("La cantidad de autos rosas son: " + roja);
+        alert("La cantidad de autos rosas son: " + rosa);
         alert("La cantidad de autos rojos son: " + roja);
         alert("La cantidad de autos verdes son: " + verde);
         alert("La cantidad de autos azules son: " + azul);
+        break;
+    case 2:
+        const seleccion = prompt("Que animal escogera entre estos tres \n1-Elefantes \n2-Jirafas \n3-chimpancés");
+        let total = 0;
+        let edad;
+        let categoria_1 = 0;
+        let categoria_2 = 0;
+        let categoria_3 = 0;
+        let animal;
+        let contador2 = 1;
+
+        if (seleccion >= 1 && seleccion <= 3) {
+            if (seleccion == 1) {
+                animal = "Elefantes";
+                total = 20;
+            } else if (seleccion == 2) {
+                animal = "Jirafas";
+                total = 15;
+            } else if (seleccion == 3) {
+                animal = "Chimpaces";
+                total = 40;
+            } else {
+                alert('Ingrese uno de los tres animales, por favor');
+            }
+            
+            for (contador2 = 1; contador2 <= total; contador2++) {
+                edad = prompt("ingresa la edad #" + contador2);
+                if (edad >= 0 && edad <= 1) {
+                    categoria_1++;
+                } else if (edad < 3) {
+                    categoria_2++;
+                } else {
+                    categoria_3++;
+                }
+            }
+        } else {
+            alert("porfavor ingrese del 1 al 3.");
+        }
+
+        let resultCategoria1 = ((categoria_1 / total) * 100).toFixed(2);
+        let resultCategoria2 = ((categoria_2 / total) * 100).toFixed(2);
+        let resultCategoria3 = ((categoria_3 / total) * 100).toFixed(2);
+
+        alert("Porcentaje de edades de " + animal + " " + resultCategoria1 + "% de 0 a 1 año.");
+        alert("Porcentaje de edades de " + animal + "\n" + resultCategoria2 + "% de mas de un 1 año y de menos de 3 años.");
+        alert("Porcentaje de edades de " + animal + "\n" + resultCategoria3 + "% de 3 años en adelante.");
+
         break;
     default:
 
