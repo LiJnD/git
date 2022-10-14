f733a39229d7769578588f9b1179deaaf3f8bbb0
LiJnD <luico199@gmal.com>
2022-10-13 23:01:05 -0500

    Punto9

--- numero.js
+++ numero.js
@@ -2,7 +2,7 @@ const actividad = Number(prompt("Escoja unas de las actvidades del 1 al 15"));
 
 switch (actividad) {
     case 1:
-        let autos = prompt("ingrese la cantida de autos");
+        let autos = prompt("Ingrese la cantida de autos");
         alert("Guardado");
         let amarillo = 0;
         let rosa = 0;
@@ -12,8 +12,7 @@ switch (actividad) {
         let contador1 = 1;
         function calculo1() {
             while (autos > "0") {
-                const placas = prompt("Ingresa el ultimo digito de la placa. #" + contador1);
-                alert("siguiente")
+                const placas = prompt("Ingrese el ultimo digito de la placa. #" + contador1);
                 if (placas == 1 || placas == 2) {
                     amarillo++;
                 } else if (placas == 3 || placas == 4) {
@@ -29,7 +28,7 @@ switch (actividad) {
                     azul++;
 
                 } else {
-                    alert("Ingrese un dijito correcto")
+                    alert("¡Ingrese un dijito correcto!");
                     autos++;
                 }
                 contador1++;
@@ -37,14 +36,15 @@ switch (actividad) {
             }
         }
         calculo1();
-        alert("La cantidad de autos amarillos son: " + amarillo);
-        alert("La cantidad de autos rosas son: " + rosa);
-        alert("La cantidad de autos rojos son: " + roja);
-        alert("La cantidad de autos verdes son: " + verde);
-        alert("La cantidad de autos azules son: " + azul);
+        alert("La cantidad de autos amarillos son: " + amarillo + ".");
+        alert("La cantidad de autos rosas son: " + rosa + ".");
+        alert("La cantidad de autos rojos son: " + roja + ".");
+        alert("La cantidad de autos verdes son: " + verde + ".");
+        alert("La cantidad de autos azules son: " + azul + ".");
         break;
     case 2:
-        const seleccion = prompt("Que animal escogera entre estos tres \n1-Elefantes \n2-Jirafas \n3-chimpancés");
+        const seleccion = prompt("Escoga un animal entre estos tres: \n1-Elefantes \n2-Jirafas \n3-chimpancés");
+        alert("¡Guardado!");
         let total = 0;
         let edad;
         let categoria_1 = 0;
@@ -64,11 +64,11 @@ switch (actividad) {
                 animal = "Chimpaces";
                 total = 40;
             } else {
-                alert('Ingrese uno de los tres animales, por favor');
+                alert('¡Ingrese uno de los tres animales, por favor!');
             }
 
             for (contador2 = 1; contador2 <= total; contador2++) {
-                edad = prompt("ingresa la edad #" + contador2);
+                edad = prompt("Ingrese la edad #" + contador2);
                 if (edad >= 0 && edad <= 1) {
                     categoria_1++;
                 } else if (edad < 3) {
@@ -78,7 +78,7 @@ switch (actividad) {
                 }
             }
         } else {
-            alert("porfavor ingrese del 1 al 3.");
+            alert("¡Porfavor ingrese, del 1 al 3!");
         }
 
         let resultCategoria1 = ((categoria_1 / total) * 100).toFixed(2);
@@ -96,17 +96,18 @@ switch (actividad) {
         let horas;
         let horas_extras;
         let salario;
+        alert("¡Guardado!");
 
         function calculo3() {
             while (contador3 <= trabajadores) {
-                horas = prompt("ingrese el numero de horas trabajadas del trabajador #" + contador3);
+                horas = prompt("Ingrese el numero de horas trabajadas del trabajador #" + contador3);
                 if (horas <= 40) {
                     salario = horas * 20;
                 } else {
                     horas_extras = horas - 40;
                     salario = 40 * 20 + (horas_extras * 25);
                 }
-                alert("El salario del trabajador " + contador3 + " es $" + salario);
+                alert("El salario del trabajador " + contador3 + " es $" + salario + ".");
                 contador3++;
             }
         }
@@ -123,26 +124,28 @@ switch (actividad) {
         let numeroMujeres = 0;
         let promedioHombres = 0;
         let promedioMujeres = 0;
+        alert("¡Guardado!");
+
         function calculo4() {
             while (contador4 <= cantidadAlumno) {
                 sexo = prompt("Ingrese el sexo #" + contador4 + "\n1-Hombre \n2-Mujer");
                 edad4 = Number(prompt("Ingrese la edad #" + contador4));
                 if (sexo == 1 || sexo == "Hombre" || sexo == "hombre") {
-                    edadHombres = edadHombres + edad4;
+                    edadHombres += edad4;
                     numeroHombres++;
                     promedioHombres = edadHombres / numeroHombres;
                 } else if (sexo == 2 || sexo == "Mujer" || sexo == "mujer") {
-                    edadMujeres = edadMujeres + edad4;
+                    edadMujeres += edad4;
                     numeroMujeres++;
                     promedioMujeres = edadMujeres / numeroMujeres;
                 } else {
-                    alert("Ingrese uno de los dos sexo");
+                    alert("¡Ingrese uno de los dos sexo!");
                 }
                 contador4++;
             }
-            alert("El promedio del grupo de alumnos es: " + (edadMujeres + edadHombres) / cantidadAlumno + " años");
-            alert("El promedio del grupo de hombres es: " + promedioHombres + " años");
-            alert("El promedio del grupo de mujeres es: " + promedioMujeres + " años");
+            alert("El promedio del grupo de alumnos es: " + (edadMujeres + edadHombres) / cantidadAlumno + " años.");
+            alert("El promedio del grupo de hombres es: " + promedioHombres + " años.");
+            alert("El promedio del grupo de mujeres es: " + promedioMujeres + " años.");
         }
         calculo4();
         break;
@@ -151,6 +154,8 @@ switch (actividad) {
         let contador5 = 1;
         let numeroMenor = 0;
         let numero;
+        alert("¡Guardado!");
+
         function calculo5() {
             while (contador5 <= total5) {
                 numero = prompt("Ingrese un numero #" + contador5);
@@ -161,7 +166,7 @@ switch (actividad) {
                 }
                 contador5++;
             }
-            alert("El numero menor es: " + numeroMenor);
+            alert("El numero menor es: " + numeroMenor + ".");
         }
         calculo5();
         break;
@@ -174,11 +179,11 @@ switch (actividad) {
 
         function calculo6() {
             for (contador6 = 1; contador6 <= 5; contador6++) {
-                pesoAnteriro = Number(prompt("Ingresa tu peso anterior persona #" + contador6));
+                pesoAnteriro = Number(prompt("Ingrese su peso anterior persona #" + contador6));
                 suma = 0;
                 for (contador62 = 1; contador62 <= 10; contador62++) {
                     peso = Number(prompt("Ingresa el peso #" + contador62));
-                    suma = suma + peso;
+                    suma += peso;
                 }
                 if ((suma / 10) == pesoAnteriro) {
                     alert("La persona " + contador6 + " se mantiene en el peso.");
@@ -201,9 +206,9 @@ switch (actividad) {
 
         function calculo7() {
             do {
-                cantidad = Number(prompt("Ingrese la cantidad de producto"));
+                cantidad = Number(prompt("Ingrese la cantidad de productos"));
                 precio = Number(prompt("Ingrese el precio de los productos #" + contador7));
-                total7 = total7 + (cantidad * precio);
+                total7 += (cantidad * precio);
                 contador7++;
                 respuesta7 = confirm("¿Quiere agregar otro producto?");
             } while (respuesta7 == true);
@@ -223,40 +228,73 @@ switch (actividad) {
         let categoria5 = 0;
         let total8 = 0;
         function calculo8() {
-            precio8 = Number(prompt("Ingresa el precio del boleto"));
+            precio8 = Number(prompt("Ingrese el precio del boleto"));
+            alert("¡Guardado!");
+
             do {
                 edad8 = Number(prompt("Ingrese la edad"));
                 if (edad8 < 5) {
-                    alert("No se permite niños menores de 5 años.")
+                    alert("¡No se permite niños menores de 5 años!");
                 } else if (edad8 <= 14) {
                     descuento = precio8 * .35;
-                    categoria1 = categoria1 + descuento;
-                }else if( edad8 <= 19 ){
+                    categoria1 += descuento;
+                } else if (edad8 <= 19) {
                     descuento = precio8 * .25;
-                    categoria2 = categoria2 + descuento;
-                }else if( edad8 <= 45 ){
+                    categoria2 += descuento;
+                } else if (edad8 <= 45) {
                     descuento = precio8 * .10;
-                    categoria3 = categoria3 + descuento;
-                }else if( edad8 <= 65 ){
+                    categoria3 += descuento;
+                } else if (edad8 <= 65) {
                     descuento = precio8 * .25;
-                    categoria4 = categoria4 + descuento;
-                }else {
+                    categoria4 += descuento;
+                } else {
                     descuento = precio8 * .35;
-                    categoria5 = categoria5 + descuento;
+                    categoria5 += descuento;
                 }
-                alert("descuento aplicado es: $"+descuento);
-                total8 = total8 + descuento;
-                respuesta8 = confirm("Desea continuar");
+                alert("Descuento aplicado es: $" + descuento);
+                total8 += descuento;
+                respuesta8 = confirm("¿Desea continuar?");
             } while (respuesta8 == true);
-            alert("El descuento total en la categoria 1 es: $"+categoria1);
-            alert("El descuento total en la categoria 2 es: $"+categoria2);
-            alert("El descuento total en la categoria 3 es: $"+categoria3);
-            alert("El descuento total en la categoria 4 es: $"+categoria4);
-            alert("El descuento total en la categoria 5 es: $"+categoria5);
-            alert("El total a pagar es: $"+total8);
+            alert("El descuento total en la categoria 1 es: $" + categoria1);
+            alert("El descuento total en la categoria 2 es: $" + categoria2);
+            alert("El descuento total en la categoria 3 es: $" + categoria3);
+            alert("El descuento total en la categoria 4 es: $" + categoria4);
+            alert("El descuento total en la categoria 5 es: $" + categoria5);
+            alert("El total que deja de percibir es: $" + total8);
         }
         calculo8();
         break;
+    case 9:
+        let ventasArray = [];
+        let comisionesArray = [];
+        //calculo
+        for (let contador9 = 1; contador9 <= 10; contador9++) {
+            function getRandom10(max) {
+                return Math.floor(Math.random() * max);
+            }
+            ventasArray.push(Number(getRandom10(300) + 1));
+
+            if (Number(ventasArray[contador9]) < 20) {
+                comisionesArray[contador9] = "10%";
+                console.log("entro");
+            } else if (Number(ventasArray[contador9]) >= 20 && Number(ventasArray[contador9]) < 40) {
+                comisionesArray[contador9] = "15%";
+                console.log("entro");
+            } else if (Number(ventasArray[contador9]) >= 40 && ventasArray[contador9] < 80) {
+                comisionesArray[contador9] = "20%";
+                console.log("entro");
+            } else if (Number(ventasArray[contador9]) >= 80 && Number(ventasArray[contador9]) < 160) {
+                comisionesArray[contador9] = "25%";
+                console.log("entro");
+            } else {
+                comisionesArray[contador9] = "30%";
+            }
+        }
+        //muestreo
+        for (let contador9 = 1; contador9 < 10; contador9++) {
+            alert("El #" + contador9 + " vendedor vendio $" + ventasArray[contador9] + " millones \n su comision es de: $" + comisionesArray[contador9]);
+        }
+        break;
     default:
-        alert('Ingrese el numero en letras');
+        alert('¡Ingrese el numero en letras!');
 }
